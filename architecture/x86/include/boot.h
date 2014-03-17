270c93de7cb91bd91fecf24ed9a286fed7523418
ictxiangxin <ictxiangxin@gmail.com>
2014-03-17 22:20:52 +0800

    Complete boot of x86
    Can Boot Starter from device.
    Add --warn for gnu as assembler.
    Add assemble inline function: clean_screen.

--- architecture/x86/include/boot.h
+++ architecture/x86/include/boot.h
@@ -1,6 +1,11 @@
 #ifndef _BOOT_H_
 #define _BOOT_H_
 
-.equ BOOT_BASE, 0x7c00
+.equ BOOT_BASE            , 0x7c0
+.equ BOOT_OFFSET          , 0x0
+.equ STARTER_BASE         , 0x0
+.equ STARTER_OFFSET       , 0x8000
+.equ STARTER_SIZE         , 0x10
+.equ STARTER_DEVICE_OFFSET, 0x1
 
 #endif
