494c406818f8ebcb226c1fd8b868f0b01232a260
ictxiangxin <ictxiangxin@gmail.com>
2014-03-19 22:49:23 +0800

    Starter(x86) go into protected mode
    Add code to make starter(x86) can running at protected mode.
    Amend some incorrect code.

---
+++ architecture/x86/include/protect.h
@@ -0,0 +1,50 @@
+#ifndef _PROTECT_H_
+#define _PROTECT_H_
+
+/* descriptor attribute */
+#define DA_CD_R           0x0000
+#define DA_CD_RA          0x0001
+#define DA_CD_RW          0x0002
+#define DA_CD_RWA         0x0003
+#define DA_CD_RE          0x0004
+#define DA_CD_REA         0x0005
+#define DA_CD_RWE         0x0006
+#define DA_CD_RWEA        0x0007
+#define DA_CD_X           0x0008
+#define DA_CD_XA          0x0009
+#define DA_CD_XR          0x000a
+#define DA_CD_XRA         0x000b
+#define DA_CD_XC          0x000c
+#define DA_CD_XCA         0x000d
+#define DA_CD_XRC         0x000e
+#define DA_CD_XRCA        0x000f
+#define DA_SG_16TSS_A     0x0001
+#define DA_SG_LDT         0x0002
+#define DA_SG_16TSS_B     0x0003
+#define DA_SG_16CALL      0x0004
+#define DA_SG_TASK        0x0005
+#define DA_SG_16INTERRUPT 0x0006
+#define DA_SG_16TRAP      0x0007
+#define DA_SG_32TSS_A     0x0009
+#define DA_SG_32TSS_B     0x000b
+#define DA_SG_32CALL      0x000c
+#define DA_SG_32INTERRUPT 0x000e
+#define DA_SG_32TRAP      0x000f
+#define DA_DPL_0          0x0000
+#define DA_DPL_1          0x0020
+#define DA_DPL_2          0x0040
+#define DA_DPL_3          0x0060
+#define DA_P              0x0080
+#define DA_NP             0x0000
+#define DA_CD             0x0010
+#define DA_SG             0x0000
+#define DA_G_1B           0x0000
+#define DA_G_4KB          0x8000
+#define DA_32BIT          0x4000
+#define DA_16BIT          0x0000
+#define DA_4GB            0x4000
+#define DA_64KB           0x0000
+#define DA_ESP            0x4000
+#define DA_SP             0x0000
+
+#endif
---
+++ architecture/x86/include/starter.h
@@ -0,0 +1,59 @@
+#ifndef _STARTER_H_
+#define _STARTER_H_
+
+.equ GDT_ADDRESS, 0x1000
+
+/*
+ * This macro will create a new segment descriptor in GDT.
+ *
+ * When use this macro, must protect ds register.
+ * If use a group of this macros, just save ds at the first and restore it at the end.
+ *
+ * Here is the structure of segment descriptor:
+ *
+ * |---------------------------------------------------------------|
+ * | Byte7 | Byte6 | Byte5 | Byte4 | Byte3 | Byte2 | Byte1 | Byte0 |
+ * | 31..24|               |         23..0         |     15..0     |
+ * | Base  |  Attribution  |         Base          |     Limit     |
+ * |-------|---------------|---------------------------------------|
+ *        /                 \
+ * |-------------Byte6-------------|-------------Byte5-------------|
+ * | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+ * | G |D/B| 0 |AVL| Limit  19..16 | P |  DPL  | S |     TYPE      |
+ * |---------------------------------------------------------------|
+ */
+.macro load_segment_descriptor number, base, limit, attribute
+    /* go into flat address mode */
+    xorw    %ax, %ax
+    movw    %ax, %ds
+
+    /* flat address mode */
+    movw    $(\number), %di
+    shlw    $0x3, %di
+    add     $GDT_ADDRESS, %di
+
+    /* temp space from stack */
+    movw    %sp, %bp
+    subw    $0x4, %bp
+
+    /* load base */
+    movl    $(\base), (%bp)
+    movw    (%bp), %ax
+    movw    %ax, 0x2(%di)
+    movb    0x2(%bp), %al
+    movb    %al, 0x4(%di)
+    movb    0x3(%bp), %al
+    movb    %al, 0x7(%di)
+
+    /* load limit and attribute */
+    movl    $(\limit), (%bp)
+    movw    (%bp), %ax
+    movw    %ax, (%di)
+    xorw    %ax, %ax
+    movb    0x2(%bp), %ah
+    andb    $0xf, %ah
+    orw     $(\attribute), %ax
+    movw    %ax, 0x5(%di)
+.endm
+
+#endif
